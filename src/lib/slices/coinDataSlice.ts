86a2dcb9a9c6c8a2e8f2d8b12062fa806fbd1450
Ivan <ivan.sizhuk94@gmail.com>
2024-03-01 22:55:44 -0500

    typescript-fix-2

--- src/lib/slices/coinDataSlice.ts
+++ src/lib/slices/coinDataSlice.ts
@@ -10,7 +10,7 @@ export interface CoinData {
   price_change_percentage_1h_in_currency: number;
   price_change_percentage_24h_in_currency: number;
   price_change_percentage_7d_in_currency: number;
-  total_volume: string;
+  total_volume: number;
   circulating_supply: string;
   total_supply: string;
   market_cap_rank: number;
@@ -34,7 +34,7 @@ const formatNumber = (num: string | number) => {
   }
 };
 
-export const fetchCoinData = createAsyncThunk<CoinData[], void, {}>("coinData/fetchCoinData", async (_, thunkAPI) => {
+export const fetchCoinData = createAsyncThunk<CoinData[], void>("coinData/fetchCoinData", async (_, thunkAPI) => {
   try {
     const response = await fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&x_cg_demo_api_key=CG-ngyTBAk7Rz4RzH3a3JZW63Zo"
